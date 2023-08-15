9904b2e32d8998b9e75edc134d7fb96b0ddf3da9
Levent Alp Taştan <tastan@Levent-Air.home>
2023-08-15 16:40:26 +0300

    first commit fix

--- features/step_definitions/clickdontchange_steps.rb
+++
@@ -1,5 +0,0 @@
-click_dont_change_button = ClickDontChangeButton.new
-
-When("click on the \"Don't Change\" button") do
-  click_dont_change_button.perform
-end
---
+++ features/step_definitions/dontchange_steps.rb
@@ -0,0 +1,5 @@
+dontchange_page = ClickDontChangeButton.new
+
+When("click on the \"Don't Change\" button") do
+  dontchange_page.perform
+end

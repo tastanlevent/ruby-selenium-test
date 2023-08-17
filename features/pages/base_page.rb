f401d0d6cb79819b6e508f6e0e6fb92d5e4dc4de
tastanlevent <levent.tastan@kloia.com>
2023-08-17 19:01:26 +0300

    xpath to id

--- features/pages/base_page.rb
+++ features/pages/base_page.rb
@@ -1,26 +1,24 @@
 class BasePage
 
   def initialize
-    @dont_change_button_xpath = '//*[@id="nav-main"]/div[1]/div/div/div[3]/span[1]/span/input'
-    @dropdown_menu_xpath = '//*[@id="nav-link-accountList"]/div'
-    @signin_button_xpath = '//*[@id="nav-flyout-ya-signin"]/a/span'
+    @dont_change_button_class = '.a-button-input[data-action-type="DISMISS"]'
+    @dropdown_menu_id = 'nav-link-accountList'
+    @signin_button_id = 'nav-flyout-ya-signin'
   end
+
   def go_to_home_page
     visit('https://www.amazon.com')
   end
 
   def perform_dontchange_button
-    find(:xpath, @dont_change_button_xpath).click
+    find(@dont_change_button_class).click
   end
 
   def hover_over_dropdown_menu
-    element = find(:xpath, @dropdown_menu_xpath)
-    element.hover
+    find(:id, @dropdown_menu_id).hover
   end
 
   def perform_signin_button
-    find(:xpath, @signin_button_xpath).click
-    sleep(2)
+    find(:id, @signin_button_id).click
   end
-
 end
